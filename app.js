325d7b9f306d8d47d671d845ae9205c1a6c2f556
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2024-11-26 02:36:59 +0800

    [init] initialized and setup files

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('SSL testing 1,2,3');
+
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// const httpsOptions = {
+//   key: fs.readFileSync(path.join(__dirname, 'server.key')),// private key
+//   cert: fs.readFileSync(path.join(__dirname, 'server.crt'))// certificate
+// };
+
+// https.createServer(httpsOptions, app).listen(3000);
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+httpsServer.listen(3443, () => {
+    console.log('Listening on port 3000');
+});
