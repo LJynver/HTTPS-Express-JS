a9850f660347db99b17c91e8e70ea58f69c69b28
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2024-11-26 12:54:33 +0800

    [feat] tried to make it connect to a localnetwork

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 
 const port = 443;
 
@@ -19,12 +20,29 @@ app.use('/', (req, res) => {
 // };
 
 // https.createServer(httpsOptions, app).listen(3000);
+const getLocalIP = () => {
+    const networkInterfaces = os.networkInterfaces();
+    // let ipAddress = `https://${getLocalIP()}:${port}`;
+    let ipAddress = `127.0.0.1`; //in case the address can not be fetched
+    for (const interface of Object.values(networkInterfaces)) {
+        for (const interfaceDetails of interface) {
+            if (interfaceDetails.family === 'IPv4' && interfaceDetails.internal) {
+                ipAddress = interfaceDetails.address;
+                break;
+            }
+        }
+    }
+    return ipAddress;
+};
+
+const ipAddress = getLocalIP();
 
 const httpsServer = https.createServer({
     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
 }, app)
 
-httpsServer.listen(443, () => {
-    console.log(`Listening on port ${port}`);
+
+httpsServer.listen(s443, '0.0.0.0', () => {
+    console.log(`Listening on port ${ipAddress}:${port}`);
 });
